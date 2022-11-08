9215072c9d6213ee9b7008f2c7b2ea21d5cc0b06
Lewluu <iuliuantoniu@gmail.com>
2022-11-08 11:44:35 +0200

    loggin update 1

---
+++ app/src/Logger/WGM_Logger.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+    Class for logging outputs error, warning or important 
+    messages in alert windows or a new tab
+*/
+
+// check session status
+if(session_status() == PHP_SESSION_NONE)    session_start();
+
+class WGM_Logger{
+    
+}
+
+?>
